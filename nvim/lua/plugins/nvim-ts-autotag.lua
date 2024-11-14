0c2b5b3a0de6150a4e0f31021adbd400aa3ac9b4
rolle_dev <89302745+toanhhg123@users.noreply.github.com>
2024-11-14 18:27:24 +0700

    fix: auto rename tag html in nvim-ts-autotag

--- nvim/lua/plugins/nvim-ts-autotag.lua
+++ nvim/lua/plugins/nvim-ts-autotag.lua
@@ -1,4 +1,18 @@
 return {
-  "windwp/nvim-ts-autotag",
-  opts = {},
+	"windwp/nvim-ts-autotag",
+	opts = {},
+	config = function()
+		require("nvim-ts-autotag").setup({
+			opts = {
+				enable_close = true,
+				enable_rename = true,
+				enable_close_on_slash = false,
+			},
+			per_filetype = {
+				["html"] = {
+					enable_close = false,
+				},
+			},
+		})
+	end,
 }
